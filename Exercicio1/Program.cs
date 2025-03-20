bc3cbb2ec27958f26809eb12d5a1c7188eaed083
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2025-03-20 20:15:01 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo valor: ");
+            string num2 = Console.ReadLine();
+
+            int numero1 = int.Parse(num1);
+            int numero2 = int.Parse(num2);
+
+            Console.WriteLine("A soma dos valores é: " +(numero1 + numero2));
+
+            
+
+
+
+        }
+    }
+}
